e97f7e5ded5a284df36521aaa3f5eb1cef4fc4f7
zahra-haghkesh <zahra.haghkesh1994@gmail.com>
2025-03-02 16:12:00 +0330

    add true&false

---
+++ boolean.js
@@ -0,0 +1,33 @@
+/** @format */
+
+//This program needs some variables to log the expected result.
+
+let weWillWin = true;
+let APSIsGreat =true;
+let AmirIsCool =true;
+let calculationCorrect =false;
+let just1Student =false;
+
+/* 
+  ---------------------------
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- 
+
+*/
+
+console.log('will we win?', weWillWin);
+console.log('Is APS great?', APSIsGreat);
+console.log('Is Amir cool?', AmirIsCool);
+console.log('1 + 1 = 5?', calculationCorrect);
+console.log('APS includes just 1 student?', just1Student);
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  will we win? true
+  Is APS great? true
+  Is Amir cool? true
+  Does 1 + 1 = 5? false
+  Are there more than 10 students? false
+  we
+*/
